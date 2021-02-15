60eb8af26f2d72ad0ad8b6e76edaa0fab5b66e12
Kevin Benny <mekevin.ben2000@gmail.com>
2021-02-15 22:48:13 +0400

    loaded libraries

---
+++ main_script.R
@@ -0,0 +1,12 @@
+#importing necessary libraries
+library(shiny) 
+library(shinydashboard)
+library(DT)
+library(fs)
+library(wbstats)
+library(leaflet)
+library(plotly)
+library(tidyverse)
+
+#utis script
+source('src_utils.R')
